1dd99f9c2ab9405dcfa8fb31cf64138f9f0808e7
Vedantaz <muleyvedant4@gmail.com>
2024-10-01 22:55:02 +0530

    added the jsx files to work frontend

---
+++ frontend/src/left/Left.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Left() {
+  return (
+    <>
+      <div className="border border-white w-[30%] bg-black text-white">
+        Left
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/right/Right.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Right() {
+  return (
+    <>
+      <div className="border border-white w-[70%] bg-purple text-white">
+        right
+      </div>
+    </>
+  );
+}
